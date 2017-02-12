5fffe3237ef640b4fbad4fce8665cef8a073d2c7
Chris Tyler <jchristyler@gmail.com>
2017-02-11 21:22:43 -0600

    Primes and BigIntPrime classes converted

---
+++ nSphereC/BigIntPrime.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace nSphereC
+{
+    class BigIntPrime
+    {
+        public BigInteger value;
+        public UInt32 prime;
+        public BigIntPrime(BigInteger value, UInt32 prime)
+        {
+            this.value = value;
+            this.prime = prime;
+        }
+        public BigIntPrime(UInt32 value)
+        {
+            this.value = value;
+            prime = Primes.LargestPrime(value);
+        }
+        public override string ToString()
+        {
+            return value.ToString() + " (" + prime.ToString() + ")";
+        }
+        public static implicit operator BigIntPrime(uint value)
+        {
+            return new BigIntPrime(value);
+        }
+        public static BigIntPrime operator *(BigIntPrime left, BigIntPrime right)
+        {
+            return new BigIntPrime(left.value * right.value, left | right);
+        }
+        public static BigIntPrime operator /(BigIntPrime left, BigIntPrime right)
+        {
+            BigInteger value = left.value / right.value;
+            uint prime = Primes.LargestPrime(value, left | right);
+            return new BigIntPrime(value, prime);
+        }
+        public static uint operator |(BigIntPrime left, BigIntPrime right)
+        {
+            return left.prime < right.prime ? right.prime : left.prime;
+        }
+    }
+}
---
+++ nSphereC/Primes.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace nSphereC
+{
+    public static class Primes
+    {
+        public static List<uint> pCache;
+        public static uint LargestPrime(uint composite)
+        {
+            if (composite == 1 || pCache.Contains(composite)) return composite;
+            int start = pCache.IndexOf(pCache.LastOrDefault(p => p <= composite / 2));
+            if (start < 0) return composite;
+            for (int i = start; i >= 0; i--)
+            {
+                if (composite % pCache[i] == 0) return pCache[i];
+            }
+            return composite;
+        }
+        public static uint LargestPrime(BigInteger composite, uint OldLargestPrime)
+        {
+            if (composite == 1) return 1;
+            for(int i = pCache.IndexOf(OldLargestPrime); i >= 0; i--)
+            {
+                if (composite % pCache[i] == 0) return pCache[i];
+            }
+            return 1;
+        }
+    }
+}
